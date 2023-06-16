a45b2490c1c2b1675351d2797c4d30376e0193bf
eerbaa <136806327+eerbaa@users.noreply.github.com>
2023-06-16 15:34:00 +0100

    Update Heat equation.py

--- Heat equation.py
+++ Heat equation.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[28]:
-
-
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jun 13 11:57:54 2023
@@ -18,7 +12,7 @@ import scipy.interpolate as spip
 
 m = 0.1
 alpha = 1
-phi = 1
+phi = 10
 
 #Framework
 
@@ -30,16 +24,19 @@ def mesh_r(start, stop, num):
 def Th(t):
     return(100) #to change later
 
-def T(r, T0):
-    return(spip.interp1d(np.transpose(T0), r))
+def Temp(T0, mesh):
+    T0_loc = np.transpose(T0)
+    T0_loc = np.reshape(T0_loc, (mesh.size))
+    return((spip.interp1d(mesh, T0_loc)))
    #return(1) #to change later
 
-def g(Temp):
+def g(Tem):
     return(1) #to change later 
 
 def E(mesh, t, T0):
+    T = Temp(T0, mesh)
     def dEdr(r, E):
-        return((-alpha**2)*np.sqrt(((T(r,T0)**3)*((E+m)**3))/((E*2)+m*E))*(1-(m/(E+m))**2)**2)
+        return((-alpha**2)*np.sqrt(((T(r)**3)*((E+m)**3))/((E*2)+m*E))*(1-(m/(E+m))**2)**2)
     y0 = np.array([Th(t)])
     sol = spi.solve_ivp(dEdr,[0,mesh[mesh.size-1]], y0, 'RK45', mesh)
     return(sol.y)
@@ -47,47 +44,51 @@ def E(mesh, t, T0):
    
 #test
 
-mesh = mesh_r(0, 50,101)
+mesh = mesh_r(0, 53,101)
 t = 0
 T0 = np.ones((1,101))
-print()
+#T = Temp(T0, mesh)
+#plt.plot(mesh, T(mesh))
+#print(T(2.6))
+
 #plot
 
 E1 = E(mesh,t, T0)
-plt.plot( mesh_r(0,30,100),np.transpose(E1))
+plt.plot(mesh,np.transpose(E1))
 plt.show()
 
-
-# In[ ]:
-
-
 def Te(T0, dt, t, mesh): #T0 is a np.array of length num
+    num = mesh.size
     if T0.size != num :
         raise Exception('The size of T0 should be num.')
     if num%2 == 0 :
         raise Exception('num must be odd')
-    else
+    else :
         r = mesh
-        Tdt = np.zeros(1,num)
-        dTdr = np.zeros(1, num) #we impose periodic boundary conditions 
-        dTdr2 = np.zeros(1,num)
-        dTdr2[0] = (T0[1]-2*T0[0]+T[num-1])/((r[1]-r[0])**2)
-        dTdr2[num-1] = (T0[0]-2*T0[num-1]+T0[num-2])/((r[num-1]-r[num-2])**2)
-        dTdr[1:num-1] = (T0[2:num]-T0[0:num-2])/(r[2:num]-r[0:num-2])
-        dTdr2[1:num-1] = (T0[2:num]-2*T0[1:num-1]+T0[0:num-2])/(((r[2:num]-2*r[1:num-1]+r[0:num-2])/2)**2)
-        E0 = np.zeros(1,num)
-        E0[0:((num+1)/2)] = np.flip(E(rmin, rmax, num,t,T0[num/2])[0:(num+1)/2])
-        E0[(num+1)/2:num] = E(rmin, rmax, num,t,T0[num/2])[1:(num+1)/2]
+        Tdt = np.zeros((1,num))
+        dTdr = np.zeros((1, num)) #we impose periodic boundary conditions 
+        dTdr2 = np.zeros((1,num))
+        dTdr2[0][0] = (T0[0][1]-2*T0[0][0]+T0[0][num-1])/((r[1]-r[0])**2)
+        dTdr2[0][num-1] = (T0[0][0]-2*T0[0][num-1]+T0[0][num-2])/((r[num-1]-r[num-2])**2)
+        dTdr[0][1:num-1] = (T0[0][2:num]-T0[0][0:num-2])/(r[2:num]-r[0:num-2])
+        dTdr2[0][1:num-1] = (T0[0][2:num]-2*T0[0][1:num-1]+T0[0][0:num-2])/(((r[2:num]-r[0:num-2])/2)**2)
+        E0 = np.zeros((1,num))
+        E0[0][0:((num+1)//2)] = np.flip(E(r,t,T0)[0][0:(num+1)//2])
+        E0[0][(num+1)//2:num] = E(r,t,T0)[0][1:(num+1)//2]
         Tdt = ((45/(((np.pi*alpha)**2)*T0*g(T0)))*dTdr +
                (45*(r**2)/(((np.pi*alpha*T0)**2)*g(T0)))*(dTdr)**2 +
                (30*(r**2)/(((np.pi*alpha)**2)*T0*g(T0)))*dTdr2 + 
-               ((15*phi*(alpha)**2)/(8*((np.pi*T0)**3)*g(T0)))*np.sqrt(((T0*(E0+m))**3)/(E0**2 + m*E0))*((1-(m/(E+m))**2)**2)
+               ((15*phi*(alpha)**2)/(8*((np.pi*T0)**3)*g(T0)))*np.sqrt(((T0*(E0+m))**3)/(E0**2 + m*E0))*((1-(m/(E0+m))**2)**2)
               )*dt + T0
         return(Tdt)
     
 #test
 
-Tdt1 = Te()
-            
-            
+dt=0.1
+Tdt1 = Te(T0, dt, t, mesh)
+T = Temp(Tdt1, mesh)
+plt.plot(mesh, T(mesh))
 
+
+            
+            
